c198a1cecb3db98929d5991410aa1f8c5f8bb261
Tanvir Rudro <rudro987@gmail.com>
2024-05-21 18:21:31 +0600

    basic server setup completed

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+const app : Application = express();
+
+//parsers
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Appolo level-2 assignment 2 server!")
+});
+
+export default app;
